6af7f3cb054275a1ff7a937c7aae97c90b782f8d
erim <jakob@erim.se>
2018-05-31 22:06:02 +0200

    Drone done!

---
+++ Tello.Net/Drone.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net.Sockets;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace Tello.Net
+{
+    public class Drone
+    {
+        private readonly Thread cmdThread = new Thread(CommandThreadMain);
+        private readonly Thread videoThread = new Thread(VideoThreadMain);
+        private readonly UdpClient cmdClient;
+        private readonly UdpClient videoClient;
+
+        public Drone()
+        {
+
+        }
+
+        private static void CommandThreadMain()
+        {
+
+        }
+
+
+        private static void VideoThreadMain()
+        {
+
+        }
+    }
+}
